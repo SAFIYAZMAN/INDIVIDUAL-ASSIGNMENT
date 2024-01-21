07fa8a448ab582aa8551292a73b747d2208186e4
SAFIYAZMAN <146957390+SAFIYAZMAN@users.noreply.github.com>
2024-01-22 07:14:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+
+
+nextBtn.addEventListener("click", () => {
+    let cards = document.querySelectorAll(".card");
+
+    //repositioning first card to last
+    document.querySelector(".slide").appendChild(cards[0]);
+
+});
+
+
+prevBtn.addEventListener("click", () => {
+    let cards = document.querySelectorAll(".card");
+
+    //repositioning last card to first
+    document.querySelector(".slide").prepend(cards[cards.length - 1]);
+
+});
+
+
+//--------End--------\\
+
+
